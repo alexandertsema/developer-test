72669bad0e2caf655812cfd322b93024ca25cf21
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 14:18:14 -0400

    Added BookViewingCommand class

---
+++ OrangeBricks.Web/Controllers/Viewing/Commands/BookViewingCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrangeBricks.Web.Controllers.Viewing.Commands
+{
+    public class BookViewingCommand
+    {
+        public string BuyerId { get; set; }
+        [Required]
+        public int PropertyId { get; set; }
+        [Required]
+        public DateTime ViewingDate { get; set; }
+        [Required]
+        public TimeSpan ViewingTime { get; set; }
+    }
+}
