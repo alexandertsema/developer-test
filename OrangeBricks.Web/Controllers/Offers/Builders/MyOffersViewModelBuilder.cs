923113a1ed882cb0a7db618a3d91876e4ad750ac
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 00:03:35 -0400

    Added MyOffersViewModelBuilder class

---
+++ OrangeBricks.Web/Controllers/Offers/Builders/MyOffersViewModelBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using OrangeBricks.Web.Controllers.Offers.ViewModels;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Offers.Builders
+{
+    public class MyOffersViewModelBuilder
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public MyOffersViewModelBuilder(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public MyOffersViewModel Build(string buyerId)
+        {
+            var offers = _context.Offers
+                .Where(x => x.BuyerId.Equals(buyerId) && x.Status == OfferStatus.Accepted);
+
+            return new MyOffersViewModel
+            {
+                Offers = offers.Select(x => new OfferViewModel()
+                {
+                    Id = x.Id,
+                    Amount = x.Amount,
+                    CreatedAt = x.CreatedAt,
+                    IsPending = x.Status == OfferStatus.Pending,
+                    Status = x.Status.ToString(),
+                    Property = x.Property
+                })
+            };
+        }
+    }
+}
