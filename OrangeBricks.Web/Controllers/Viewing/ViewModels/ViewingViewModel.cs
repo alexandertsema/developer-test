4e0c5b36eeab365caab501016d5f96cc45c91f98
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 15:36:49 -0400

    Added ViewingViewModel class

---
+++ OrangeBricks.Web/Controllers/Viewing/ViewModels/ViewingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using OrangeBricks.Web.Controllers.Property.ViewModels;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Viewing.ViewModels
+{
+    public class ViewingViewModel
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Required]
+        public DateTime ViewAt { get; set; }
+        public PropertyViewModel Property { get; set; }
+        [Required]
+        public int PropertyId { get; set; }
+        public string BuyerUserName { get; set; }
+        [Required]
+        public string BuyerId { get; set; }
+    }
+}
