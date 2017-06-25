783a21fb136b8b3aa61863b82e134d3e7a40dbf6
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 13:14:32 -0400

    Added AvailableViewingsViewModelBuilder class with not impemented methods

---
+++ OrangeBricks.Web/Controllers/Viewing/Builders/AvailableViewingsViewModelBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using OrangeBricks.Web.Controllers.Viewing.ViewModels;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Viewing.Builders
+{
+    public class AvailableViewingsViewModelBuilder
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public AvailableViewingsViewModelBuilder(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public AvailableViewingsViewModel Build(int propertyId, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.Viewing> GetViewingsOfDate(DateTime date, int propertyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SelectListItem> GenerateSchedule(TimeSpan startTime, TimeSpan endTime, int viewingDuration, DateTime date, IList<Models.Viewing> viewingsOnDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
