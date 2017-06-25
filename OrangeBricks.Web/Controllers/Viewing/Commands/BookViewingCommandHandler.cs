9f46faa569eabf2e132ffd86a026bab2c19f30db
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 14:31:27 -0400

    Added BookViewingCommandHandler class -Added Handle method to BookViewingCommandHandler class

---
+++ OrangeBricks.Web/Controllers/Viewing/Commands/BookViewingCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Viewing.Commands
+{
+    public class BookViewingCommandHandler
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public BookViewingCommandHandler(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public bool Handle(BookViewingCommand command)
+        {
+            if (_context.Viewing.Any(x => x.BuyerId.Equals(command.BuyerId)
+                                          && x.PropertyId.Equals(command.PropertyId)
+                                          && DbFunctions.TruncateTime(x.ViewAt) ==
+                                          DbFunctions.TruncateTime(command.ViewingDate)))
+            {
+                return false;
+            }
+
+            var vewing = new Models.Viewing
+            {
+                ViewAt = command.ViewingDate + command.ViewingTime,
+                BuyerId = command.BuyerId,
+                PropertyId = command.PropertyId
+            };
+
+            _context.Viewing.Add(vewing);
+
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
