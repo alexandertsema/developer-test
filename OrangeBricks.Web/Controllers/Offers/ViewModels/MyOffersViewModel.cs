7d834d0c3dceaee8135af7e2364669e7352a54a1
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-24 23:59:53 -0400

    Added MyOffersViewModel class

---
+++ OrangeBricks.Web/Controllers/Offers/ViewModels/MyOffersViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OrangeBricks.Web.Controllers.Offers.ViewModels
+{
+    public class MyOffersViewModel
+    {
+        public IEnumerable<OfferViewModel> Offers { get; set; }
+    }
+}
