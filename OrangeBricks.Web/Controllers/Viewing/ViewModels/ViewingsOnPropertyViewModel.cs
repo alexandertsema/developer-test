0ac8d4b6c95d42e6e34d6c47134d3b40e986d6e9
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 15:38:33 -0400

    Added ViewingsOnPropertyViewModel class

---
+++ OrangeBricks.Web/Controllers/Viewing/ViewModels/ViewingsOnPropertyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OrangeBricks.Web.Controllers.Viewing.ViewModels
+{
+    public class ViewingsOnPropertyViewModel
+    {
+        public IEnumerable<ViewingViewModel> Viewings { get; set; }
+    }
+}
