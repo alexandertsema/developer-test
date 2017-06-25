ea11d20c936d8aeaee5663c48815cda82f5cd1c4
ALEXANDER-DESKT\Alex <at.webdes@gmail.com>
2017-06-25 13:11:36 -0400

    Added AvailableViewingsViewModel which contains schedule with available appointment times

---
+++ OrangeBricks.Web/Controllers/Viewing/ViewModels/AvailableViewingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace OrangeBricks.Web.Controllers.Viewing.ViewModels
+{
+    public class AvailableViewingsViewModel
+    {
+        public int PropertyId { get; set; }
+
+        public string StartViewingDate { get; set; }
+
+        [DisplayName("Viewing Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime ViewingDate { get; set; }
+        public TimeSpan ViewingTime { get; set; }
+
+        [DisplayName("Viewing Time")]
+        public IEnumerable<SelectListItem> Times { get; set; }
+
+        public int ViewingDuration { get; set; }
+    }
+}
